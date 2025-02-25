a04d8b56111607ea6633f5439b00ac70dc1b17b1
Shreyas S D <shreyas.dash@s.amity.edu>
2025-02-25 11:23:06 +0530

    maxandmin.py

---
+++ maxandmin.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = int(input("Enter third number"))
+if a > b > c :
+    print(a, "is maximum")
+if b > a > c :
+    print(b, "is maximum")
+if c > a > b :
+    print(c, "is maximum")
+if a < b < c :
+    print(a, "is minimum")
+if b < a < c :
+    print(b, "is minimum")
+if c < a < b :
+    print(c, "is minimum")
