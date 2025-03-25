60a53309ef259f0d3e3b831f103da4eb73ea3770
Shreyas S D <shreyas.dash@s.amity.edu>
2025-03-25 11:33:05 +0530

    first switch

---
+++ btech.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    
+    printf("Menu:\n");
+    printf("1. Btech CSE\n");
+    printf("2. Btech AIML\n");
+    printf("3. Btech Electrical\n");
+    printf("4. Btech Mechanical\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            printf("You have chosen Btech CSE\n");
+            break;
+            return 0;
+        default:
+            printf("Invalid choice! Please enter a number between 1 and 4.\n");
+    }
+    
+    return 0;
+}
